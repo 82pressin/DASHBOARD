6453c6ab41075510b0d2c4e9fcb49894bddaf914
82pressin <intaanfdh@gmail.com>
2026-05-02 18:16:53 +0700

    7th commit here comes in

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 from data import *
 
 def judul():
-          st.title("Dashboard COVID-19 🦠")
+          st.title("DASHBOARD COVID-19 🦠")
           st.write("Selamat datang di dashboard interaktif untuk menganalisis data COVID-19 di Indonesia! ")
       
-st.sidebar.title("Menu")
-menu = st.sidebar.radio("Pilih Menu", ("Home", "Data"))
+st.sidebar.title("MENU")
+menu = st.sidebar.radio("Pilih Menu:", ("Home", "Data"))
 
 if menu == "Home":
           judul()
--- data.py
+++ data.py
@@ -59,38 +59,38 @@ def kolom(df):
 
 
 def pie_chart(df):
-          total_deaths = df['Total Deaths'].sum()
-          total_recoveries = df['Total Recovered'].sum()
-          
-          data = {
-                    'Status' : ['Total Kematian', 'Total Kesembuhan'],
-                    'Jumlah' : [total_deaths, total_recoveries]}
-          
-          fig = px.pie(data, 
-                       names='Status', values='Jumlah', 
-                       title='Distribusi Kasus COVID-19 di Indonesia', 
-                       hole = 0.5, color_discrete_sequence = ['#4ECDC4', '#FF6B6B'])
-          
-          st.plotly_chart(fig, use_container_width=True)
+    st.markdown("#### Distribusi Kasus COVID-19 di Indonesia")
+    total_deaths = df['Total Deaths'].sum()
+    total_recoveries = df['Total Recovered'].sum()
+    
+    data = {'Status' : ['Total Kematian', 'Total Kesembuhan'],
+            'Jumlah' : [total_deaths, total_recoveries]}
+    
+    fig = px.pie(data, names='Status', values='Jumlah', 
+                 hole = 0.5, color_discrete_sequence = ['#4ECDC4', '#FF6B6B'])
+    
+    st.plotly_chart(fig, use_container_width=True)
 
 def bar_chart1(df):
+    st.markdown("#### 5 Provinsi dengan Total Kematian Tertinggi")
+        
     df_last = df.sort_values('Date').groupby('Location', as_index=False).last()
     top5 = df_last.nlargest(5, 'Total Deaths')
     
     fig = px.bar(top5, x='Location', y='Total Deaths',
-                 title='5 Provinsi dengan Total Kematian Tertinggi',
                  color='Total Deaths', color_continuous_scale='Reds',
                  labels={'Total Deaths': 'Total Kematian', 'Location': 'Provinsi'})
     
     fig.update_layout(xaxis_title='Provinsi', yaxis_title='Total Kematian')
     st.plotly_chart(fig, use_container_width=True)
-
+    
 def bar_chart2(df):
+    st.markdown("#### 5 Provinsi dengan Total Kesembuhan Tertinggi")
+    
     df_last = df.sort_values('Date').groupby('Location', as_index=False).last()
     top5 = df_last.nlargest(5, 'Total Recovered')
     
     fig = px.bar(top5, x='Location', y='Total Recovered',
-                 title='5 Provinsi dengan Total Kesembuhan Tertinggi',
                  color='Total Recovered', color_continuous_scale='Greens',
                  labels={'Total Recovered': 'Total Kesembuhan', 'Location': 'Provinsi'})
     
@@ -98,6 +98,8 @@ def bar_chart2(df):
     st.plotly_chart(fig, use_container_width=True)
 
 def map_chart(df, year = None):
+    st.markdown(f'#### Peta Sebaran Kasus COVID-19 di Indonesia ({year if year else "Semua Tahun"})')
+    
     df['Date'] = pd.to_datetime(df['Date'])
     
     if year:
@@ -113,8 +115,7 @@ def map_chart(df, year = None):
     fig = px.scatter_mapbox(df_map, lat='Latitude', lon='Longitude', size='Total Cases',
                             color='Total Cases', hover_name='Location', zoom=4, 
                             center={'lat': -2.5, 'lon': 115}, size_max=20, opacity=0.7, 
-                            color_continuous_scale='OrRd', 
-                            title=f'Peta Sebaran Kasus COVID-19 di Indonesia ({year if year else "Semua Tahun"})')
+                            color_continuous_scale='OrRd')
     
     fig.update_layout(mapbox_style='open-street-map', height=400, margin={'r':0,'t':30,'l':0,'b':0})
     
